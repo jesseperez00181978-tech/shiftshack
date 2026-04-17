fee5f9dda7378218445562292d33f3b7c99dd847
jesseperez00181978-tech <jesseperez00181978@gmail.com>
2026-04-16 19:59:40 -0600

    package.json

---
+++ main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>ShiftStack 💼</h1>
+      <p>Your job tracker is alive.</p>
+    </div>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
